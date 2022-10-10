a1ee8e5cb12cab9380274b605f321fe196558372
Gustakx36 <102194654+Gustakx36@users.noreply.github.com>
2022-10-10 16:08:18 -0300

    Update DataPorExten.java

--- DataPorExten.java
+++ DataPorExten.java
@@ -2,7 +2,18 @@
 public class DataPorExten {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Qual a data lembre de colocar barra (/) : ");
+		String data = sc.next();
+		String[] datanormal = data.split("/");
+		int datames = Integer.parseInt(datanormal[1]);
+		String[]  dataextenso = {"JANEIRO","FEVEREIRO","MARÇO","ABRIL","MAIO","JUNHO","JULHO","AGOSTO","SETEMBRO","OUTUBRO","NOVEMBRO","DEZEMBRO",};
+
+		System.out.println("Hoje é dia " + datanormal[0] + " de " + dataextenso[datames - 1] + " de " + datanormal[2]);
+		
+		sc.close();
 
 	}
 
